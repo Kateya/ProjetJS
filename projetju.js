37cf9afa035dd7ed2d7273b3d5b06e1b54128976
juliedi <julie.di@codeur.online>
2017-10-19 16:03:32 +0200

    toutjulie

---
+++ projetju.js
@@ -0,0 +1,12 @@
+var texte=document.getElementById("titre");
+
+function changeText() {
+    if(titre.style==="font-size:30px"){
+        titre.style="font-size:50px";
+    }
+    else (titre.style==="font-size:50px"){
+        titre.style="font-size:30px";
+    }
+
+}
+
